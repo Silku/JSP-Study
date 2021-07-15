bb5b8dda7ac3ca18dd95ef5a0292d44204491c73
tw <kokicola@naver.com>
2021-07-15 18:02:57 +0900

    07.15 jsp UserBean, join, joinMgrPool

---
+++ 김태우8799/src/jspEx/JoinProc.java
@@ -0,0 +1,26 @@
+package jspEx;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import db.DBConnectionMgr;
+
+/**
+ * Servlet implementation class JoinProc
+ */
+@WebServlet("/jspEx/joinProc")
+public class JoinProc extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+
+}
---
+++ 김태우8799/src/jspEx/UserBean.java
@@ -0,0 +1,34 @@
+package jspEx;
+
+public class UserBean {
+	private String id;
+	private String pwd;
+	private String name;
+	private String birthday;
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPwd() {
+		return pwd;
+	}
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getBirthday() {
+		return birthday;
+	}
+	public void setBirthday(String birthday) {
+		this.birthday = birthday;
+	}
+
+}
