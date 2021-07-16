f8cab725f24f5b8a6dcf6894eb8254222e73572e
tw <kokicola@naver.com>
2021-07-16 18:00:12 +0900

    게시판 /DBconnect bean, pool

---
+++ 김태우8799/src/jspEx/BorderMgrPool.java
@@ -0,0 +1,56 @@
+package jspEx;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+
+import db.DBConnectionMgr;
+
+public class BorderMgrPool {
+	private DBConnectionMgr pool = null;
+	
+	SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+	Date now = new Date();
+	String nowDate = dateFormat.format(now);
+	
+	public BorderMgrPool() {
+		try {
+			pool = DBConnectionMgr.getInstance();
+		}catch(Exception e) {
+			System.out.println("오류: DBConnection Pool 실패.");
+		}
+	}
+	
+	public ArrayList<BorderMstBean> getBorderList() {
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		String sql = null;
+		ArrayList<BorderMstBean> list = new ArrayList<BorderMstBean>();
+		
+		try {
+			con = pool.getConnection();
+			sql = "select * from border_mst";
+			pstmt = con.prepareStatement(sql);
+			rs = pstmt.executeQuery();
+			while(rs.next()) {
+				BorderMstBean bean = new BorderMstBean();
+				bean.setBorder_code(rs.getInt(1));
+				bean.setBorder_index(rs.getInt(2));
+				bean.setBorder_title(rs.getString(3));
+				bean.setBorder_writer(rs.getString(4));
+				bean.setBorder_date(rs.getString(5));
+				bean.setBorder_count(rs.getInt(6));
+				list.add(bean);
+			}
+		}catch (Exception e) {
+			e.printStackTrace();
+		}finally {
+			pool.freeConnection(con, pstmt, rs);
+		}
+		return list;
+	}
+}
---
+++ 김태우8799/src/jspEx/BorderMstBean.java
@@ -0,0 +1,48 @@
+package jspEx;
+
+public class BorderMstBean {
+	private int border_code;
+	private int border_index;
+	private String border_title;
+	private String border_writer;
+	private String border_date;
+	private int border_count;
+	
+	public int getBorder_code() {
+		return border_code;
+	}
+	public void setBorder_code(int border_code) {
+		this.border_code = border_code;
+	}
+	public int getBorder_index() {
+		return border_index;
+	}
+	public void setBorder_index(int border_index) {
+		this.border_index = border_index;
+	}
+	public String getBorder_title() {
+		return border_title;
+	}
+	public void setBorder_title(String border_title) {
+		this.border_title = border_title;
+	}
+	public String getBorder_writer() {
+		return border_writer;
+	}
+	public void setBorder_writer(String border_writer) {
+		this.border_writer = border_writer;
+	}
+	public String getBorder_date() {
+		return border_date;
+	}
+	public void setBorder_date(String border_date) {
+		this.border_date = border_date;
+	}
+	public int getBorder_count() {
+		return border_count;
+	}
+	public void setBorder_count(int border_count) {
+		this.border_count = border_count;
+	}
+	
+}
